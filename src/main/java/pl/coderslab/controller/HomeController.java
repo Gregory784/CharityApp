dee72716bb7580b1c9bd71c3c939cc1c0f84ee17
Grzegorz Przebinda <grzegorz.przebinda@gmail.com>
2021-06-22 15:10:35 +0200

    show institutions

--- src/main/java/pl/coderslab/controller/HomeController.java
+++ src/main/java/pl/coderslab/controller/HomeController.java
@@ -1,16 +1,20 @@
 package pl.coderslab.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.model.service.institution.InstitutionService;
 
 
 @Controller
+@AllArgsConstructor
 public class HomeController {
-
+    private final InstitutionService institutionService;
 
     @RequestMapping("/")
     public String homeAction(Model model){
+        model.addAttribute("institutions", institutionService.getInstitution());
         return "index";
     }
 }
