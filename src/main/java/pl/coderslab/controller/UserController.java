a2f1cc82b444a00bc44d1dedea60ac54e811052a
Grzegorz Przebinda <grzegorz.przebinda@gmail.com>
2021-06-28 14:55:47 +0200

    user controller, user form

---
+++ src/main/java/pl/coderslab/controller/UserController.java
@@ -0,0 +1,32 @@
+package pl.coderslab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.coderslab.model.entity.User;
+
+import javax.validation.Valid;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/adduser")
+    public String addUserGet(Model model){
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/adduser")
+    public String addUserPost(@ModelAttribute("user") @Valid User user, BindingResult bindingResult){
+        //conditions
+        if(!user.getPassword().equals(user.getPassword2())){
+            bindingResult.rejectValue("password2", "error.user", "Password are different");
+        }
+
+
+        return "login";
+    }
+}
--- src/main/java/pl/coderslab/model/entity/User.java
+++ src/main/java/pl/coderslab/model/entity/User.java
@@ -1,8 +1,18 @@
 package pl.coderslab.model.entity;
 
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "users")
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String email;
     private String password;
+    private String password2;
 }
